5d5932fbb343c56326c546d3a07ec14ad55b98ee
Nurdien Adijaya <nurdienadijaya@gmail.com>
2021-09-01 12:45:58 +0700

    try deploy

--- src/components/mystore/MyStore.js
+++ src/components/mystore/MyStore.js
@@ -23,7 +23,7 @@ const services = [
 ];
 
 const MyStore = () => {
-  const [service, setservice] = useState("EUR");
+  const [service, setservice] = useState("package");
   const handleChange = (event) => {
     setservice(event.target.value);
   };
@@ -98,43 +98,32 @@ const MyStore = () => {
           }}
         >
           <TextField
-            id="outlined-basic"
             className="textfield"
             label="Store Name*"
             variant="outlined"
           />
+          <TextField className="textfield" label="Email*" variant="outlined" />
           <TextField
-            id="outlined-basic"
-            className="textfield"
-            label="Email*"
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-basic"
             className="textfield"
             label="Contact Number*"
             variant="outlined"
           />
           <TextField
-            id="outlined-basic"
             className="textfield"
             label="Website (optional)"
             variant="outlined"
           />
           <TextField
-            id="outlined-basic"
             className="textfield"
             label="Facebook Account (optional)"
             variant="outlined"
           />
           <TextField
-            id="outlined-basic"
             className="textfield"
             label="Instagram Account (optional)"
             variant="outlined"
           />
           <TextField
-            id="outlined-basic"
             className="textfield"
             label="Twitter Account (optional)"
             variant="outlined"
